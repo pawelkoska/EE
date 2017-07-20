0717a8b3469cac0d02324fa25e3a4d0cfac51a61
pawkos <pawkos@pawkos-PC>
2017-07-21 00:37:20 +0200

    Category-Examination one-to-many implementation

---
+++ src/main/java/pl/lodz/p/it/spjava/medcenter/endpoint/ExaminationEndpoint.java
@@ -0,0 +1,46 @@
+package pl.lodz.p.it.spjava.medcenter.endpoint;
+
+import java.util.List;
+import javax.ejb.EJB;
+import javax.ejb.Stateful;
+import pl.lodz.p.it.spjava.medcenter.dto.ExaminationDTO;
+import pl.lodz.p.it.spjava.medcenter.facade.CategoryFacade;
+import pl.lodz.p.it.spjava.medcenter.facade.ExaminationFacade;
+import pl.lodz.p.it.spjava.medcenter.model.Category;
+import pl.lodz.p.it.spjava.medcenter.model.Examination;
+
+/**
+ *
+ * @author pawkos
+ */
+@Stateful
+public class ExaminationEndpoint {
+
+    @EJB
+    private ExaminationFacade examinationFacade;
+
+    @EJB
+    private CategoryFacade categoryFacade;
+
+    public void createExamination(ExaminationDTO examination) {
+
+        List<Category> categories = categoryFacade.findAll();
+        Category selectedCategory = null;
+        for (Category category : categories) {
+            if (category.getName().equals(examination.getExaminationCategory())) {
+                selectedCategory = category;
+                break;
+            }
+        }
+        if (selectedCategory == null) {
+            throw new IllegalArgumentException("Błędna nazwa kategorii"); //TODO: w tym miejscu wymagane zgłoszenie wyjątku aplikacyjnego
+        }
+
+        Examination examinationEntity = new Examination();
+        examinationEntity.setName(examination.getName());
+        examinationEntity.setExaminationDescription(examination.getExaminationDescription());  
+        examinationEntity.setExaminationCategory(selectedCategory);
+        examinationFacade.create(examinationEntity);
+    }
+
+}
